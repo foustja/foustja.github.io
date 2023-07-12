71ca03e623704f2c0e53b36eb37b732bb1c81019
Artifex <foustja@gmail.com>
2023-07-12 15:01:37 -0700

    Add files via upload

---
+++ sketch1.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400, WEBGL);
+  noStroke();
+}
+function draw() {
+  background(0);
+  pointLight(255, 255, 255, 0, -400, 400);
+  pointLight(200, 200, 200, 0, 0, 400);
+  for (let i = 0; i < TAU; i += PI / 256) {
+    push();
+    rotateZ(i);
+    translate(100, 0, 0);
+    rotateY(i + frameCount / 50);
+    box(30, 2, 30);
+    pop();
+  }
+}
