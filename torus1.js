8d55346058d6fae4925a346347628a40b5c83dc3
Artifex <foustja@gmail.com>
2025-02-17 22:27:07 -0800

    Update torus1.js

--- torus1.js
+++ torus1.js
@@ -11,6 +11,7 @@ noStroke`つぶやきProcessing`
 rotateX(.5)
 rotateY(-.5)
 r=100
-for(y=40;y--;)for(x=80;x--;pop(sphere(cos(v)+.3)))push(),translate((2+sin(v=(y+t)*p*2))*cos(u=(x+t)*p)*r,(2+sin(v))*sin(u)*r,z=cos(v)*r)
-t=(t+.04)%1}
+for(y=40;y--;)
+  for(x=80;x--;pop(sphere(cos(v)+.3)))push(),translate((2+sin(v=(y+t)*p*2))*cos(u=(x+t)*p)*r,(2+sin(v))*sin(u)*r,z=cos(v)*r)
+  t=(t+.04)%1
 }
