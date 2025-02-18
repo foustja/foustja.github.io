4cb316a563fd6fe82c2bdc3b7d55e13471b15753
Artifex <foustja@gmail.com>
2025-02-17 22:37:14 -0800

    Update torus1.js

--- torus1.js
+++ torus1.js
@@ -1,17 +1,16 @@
 function setup() {
   let myCanvas = createCanvas(400, 400, WEBGL); 
   myCanvas.parent('myContainer');
-  
+  noStroke();
 }
 function draw() {
-t=0
-background(0)
-p=PI/40
-noStroke();
-rotateX(.5)
-rotateY(-.5)
-r=100
+t=0;
+background(0);
+p=PI/40;
+rotateX(.5);
+rotateY(-.5);
+r=100;
 for(y=40;y--;)
   for(x=80;x--;pop(sphere(cos(v)+.3)))push(),translate((2+sin(v=(y+t)*p*2))*cos(u=(x+t)*p)*r,(2+sin(v))*sin(u)*r,z=cos(v)*r)
-  t=(t+.04)%1
+  t=(t+.04)%1;
 }
