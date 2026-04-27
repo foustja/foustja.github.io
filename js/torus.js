f90e8a35e59dcdd45442fcb79ceb0c2c38e122eb
foustja <foustja@gmail.com>
2026-04-27 14:47:09 -0700

    Add 3D canvas setup and draw functions

--- js/torus.js
+++ js/torus.js
@@ -1,1 +1,19 @@
 
+function setup() {
+  let myCanvas = createCanvas(400, 400, WEBGL); 
+  myCanvas.parent('myContainer');
+  noStroke();
+}
+function draw() {
+  background(0);
+  pointLight(255, 255, 255, 0, -400, 400);
+  pointLight(200, 200, 200, 0, 0, 400);
+  for (let i = 0; i < TAU; i += PI / 256) {
+    push();
+    rotateZ(i);
+    translate(100, 0, 0);
+    rotateY(i + frameCount / 50);
+    box(30, 2, 30);
+    pop();
+  }
+}
